ae7a8d92f4ce85b5574bd7694fb3f07e6e9c6da3
Budbomber <Nick_alexander@live.co.uk>
2023-12-03 19:12:03 +0000

    Add login and signup templates
    Added login.html and signup.html templates to manage user authentication. This allows users to easily login, sign up and logout. The templates include form inputs for username and password, and use Bootstrap for styling. Also, slight change in urls.py to link these templates correctly.

--- Frollab/urls.py
+++ Frollab/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path
+from apps.users.views import signup
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login.html'), name='logout'),
-    path('signup/', auth_views.SignupView.as_view(template_name='signup.html'), name='signup'),
+    path('signup/', signup, name='signup'),
 ]
