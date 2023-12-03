e9a3d5e4426d9a9e940aacc8b37bb36bf7dbe253
Budbomber <Nick_alexander@live.co.uk>
2023-12-03 20:56:24 +0000

    'website_url'. Also, a signal is set up to automatically create a UserProfile when a new User is created.
    In 'apps/users/admin.py', custom admin was established to extend the default User admin, adding a 'Task Count' field in the list display to show count of tasks assigned to each user.
    
    These changes are made to provide a richer profile for users and to provide an enhanced admin view for user management.

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+from apps.communication.models import Message
+from apps.task_management.models import Task
+from apps.users.models import UserProfile
+
 
 # Register your models here.
+
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'get_task_count')
+
+    def get_task_count(self, obj):
+        return Task.objects.filter(owner=obj).count()
+
+    get_task_count.short_description = 'Task Count'
+
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(UserProfile)
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+from apps.task_management.models import Task
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    organization = models.CharField(max_length=100, blank=True)
+    job_title = models.CharField(max_length=100, blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    bio = models.TextField(blank=True)
+    location = models.CharField(max_length=100, blank=True)
+    contact_number = models.CharField(max_length=100, blank=True)
+    website_url = models.URLField(blank=True)
+
+    def assigned_tasks(self):
+        return Task.objects.filter(assignee=self.user)
+
+    def __str__(self):
+        return f'{self.user.username.capitalize()}s Profile'
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
