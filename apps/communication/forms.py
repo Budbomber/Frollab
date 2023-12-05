4c2c3ba7f4d35960f4cf6e4a4c70f23623fe5bd7
Budbomber <Nick_alexander@live.co.uk>
2023-12-05 20:36:10 +0000

    Add compose message functionality
    A compose message feature has been added to improve communication between users. To achieve this, a `MessageForm` was introduced in `forms.py` and a corresponding view was created in `views.py`. The new html template, `compose_message.html`, presents a user-facing form for message composition. There are also changes in urls.py and dashboard.html to integrate the new feature into the existing message system.

--- apps/communication/forms.py
+++ apps/communication/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.models import User
+
 from apps.communication.models import Message
 
 
@@ -6,3 +8,25 @@ class ReplyForm(forms.ModelForm):
     class Meta:
         model = Message
         fields = ['message', 'subject', 'message']
+
+
+class MessageForm(forms.ModelForm):
+    class Meta:
+        model = Message
+        fields = ['receiver', 'subject', 'message']
+
+    def clean_receiver(self):
+        receiver_username = self.cleaned_data.get('receiver')
+        try:
+            receiver = User.objects.get(username=receiver_username)
+            return receiver
+        except User.DoesNotExist:
+            raise forms.ValidationError('User does not exist')
+
+    def save(self, commit=True):
+        message = super(MessageForm, self).save(commit=False)
+        receiver_username = self.cleaned_data['receiver']
+        message.receiver = User.objects.get(username=receiver_username)
+        if commit:
+            message.save()
+        return message
--- apps/communication/views.py
+++ apps/communication/views.py
@@ -1,11 +1,12 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.decorators.http import require_POST
 
-from apps.communication.forms import ReplyForm
+from apps.communication.forms import ReplyForm, MessageForm
 from apps.communication.models import Message
 
-
+User = get_user_model()
 @login_required
 def view_message(request, message_id):
     message = get_object_or_404(Message, id=message_id, receiver=request.user)
@@ -28,3 +29,15 @@ def delete_message(request, message_id):
     message = get_object_or_404(Message, id=message_id, receiver=request.user)
     message.delete()
     return redirect('dashboard')  # Redirect to the dashboard or appropriate page after deletion
+
+
+@login_required
+def compose_message(request):
+    form = MessageForm(request.POST or None)
+    if request.method == 'POST' and form.is_valid():
+        message = form.save(commit=False)
+        message.sender = request.user
+        message.save()
+        return redirect('dashboard')
+
+    return render(request, 'messaging/compose_message.html', {'form': form})
